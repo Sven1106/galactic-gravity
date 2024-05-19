1452f932b72e1650e148d68e631de6155b3fdf14
sven1106 <28002895+Sven1106@users.noreply.github.com>
2024-05-19 15:35:28 +0200

    Added share component

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { z, defineCollection } from 'astro:content';
+import { format } from 'date-fns';
+const postsCollection = defineCollection({
+    schema: z.object({
+        author: z.string(),
+        categories: z.array(z.string()),
+        date: z.string().transform(val => format(new Date(val), 'MMMM d, yyyy')),
+        featured: z.boolean().default(false),
+        image: z.string(),
+        title: z.string(),
+    }),
+});
+export const collections = {
+    posts: postsCollection,
+};
